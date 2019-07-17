6236afe7a9913d1213b5d8a1a083266db0d2bd28
SYChen <isnot2bad@gmail.com>
2019-07-17 20:51:13 +0800

    至binarize_per_slice

---
+++ test/preprocessing/step1.py
@@ -0,0 +1,70 @@
+"""
+测试代码
+"""
+import os
+from sample.preprocessing.step1 import load_scan, get_pixels_hu, \
+    binarize_per_slice
+from matplotlib import pyplot as plt
+import numpy as np
+
+INPUT_FOLDER = os.path.expanduser('~/DSB3/stage1/')
+
+
+def load_scan_test():
+    patients = os.listdir(INPUT_FOLDER)
+    patients.sort()
+    print("os.path.join(INPUT_FOLDER, patients[25]) =",
+          os.path.join(INPUT_FOLDER, patients[25]))
+
+    slices = load_scan(os.path.join(INPUT_FOLDER, patients[25]))
+    print(slices)
+
+
+def get_pixels_hu_test():
+    patients = os.listdir(INPUT_FOLDER)
+    patients.sort()
+    # print("os.path.join(INPUT_FOLDER, patients[25]) =",
+    #       os.path.join(INPUT_FOLDER, patients[25]))
+
+    slices = load_scan(os.path.join(INPUT_FOLDER, patients[25]))
+    case_pixels, spacing = get_pixels_hu(slices)
+    print(case_pixels)
+    print(case_pixels.shape)
+    print(case_pixels.dtype)
+    print(case_pixels.max())
+    print(case_pixels.min())
+    print(spacing)
+    print(spacing.dtype)
+    # plt.hist(
+    #     case_pixels.reshape(-1),
+    #     bins=100,
+    # )
+    # plt.title("histogram")
+    # plt.show()
+
+    plt.imshow(case_pixels[60, 50:-50, 100:-100], cmap="gray")
+    plt.show()
+    print(spacing.ndim)
+    print(spacing.shape)
+    print(len(spacing))
+
+
+def binarize_per_slice_test():
+    patients = os.listdir(INPUT_FOLDER)
+    patients.sort()
+    # print("os.path.join(INPUT_FOLDER, patients[25]) =",
+    #       os.path.join(INPUT_FOLDER, patients[25]))
+
+    slices = load_scan(os.path.join(INPUT_FOLDER, patients[25]))
+    case_pixels, spacing = get_pixels_hu(slices)
+    bw = binarize_per_slice(case_pixels, spacing)
+    print(type(bw))
+
+
+if __name__ == '__main__':
+    # load_scan_test()
+    # get_pixels_hu_test()
+    binarize_per_slice_test()
+    # X, Y = np.meshgrid([1, 2, 3], [1, 2])
+    # print(X)
+    # print(Y)
