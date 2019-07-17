9123c654feba4ed66ea9961c83890c5e39431651
SYChen <isnot2bad@gmail.com>
2019-07-17 15:56:54 +0800

    重新整理代码结构

--- training/classifier_stage1.sh
+++
@@ -1,2 +0,0 @@
-cd ./classifier
-python adapt_ckpt.py --model1  net_detector_3 --model2  net_classifier_3  --resume ../../model/detector.ckpt
--- training/detector_phase1.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-cd detector
-eps=100
-#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8 python main.py --model res18 -b 32 --epochs $eps --save-dir res18 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --model res18 -b 32 --epochs $eps --save-dir res18 -j 32 #> ../detector_phase1.txt 
-
